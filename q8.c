e0360e23a6ef82b7f171f8b6eb93f4f9051d8afa
gabrielddrs <gabrieldavid.alu.lmb@gmail.com>
2024-10-21 12:23:10 -0300

    questao 8

---
+++ q8.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+
+    int num;
+
+    printf("Digite um valor inteiro:\n");
+    scanf("%d", &num);
+
+    printf("\nO antecessor é: %d", num-1);
+    printf("\nO sucessor é: %d", num+1);
+
+
+};
