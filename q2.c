681120b40bc6979bdcebca68b04b8d772542c74a
gabrielddrs <gabrieldavid.alu.lmb@gmail.com>
2024-10-21 12:27:48 -0300

    questao 2

--- q2.c
+++ q2.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
-int main(void){
+int main(){
     float num;
     
     printf("Digite um número do tipo float:\n");
     scanf("%f", &num);
 
     printf("E o seu número com 2 pontos flutuantes é: %.2f", num);
+
+    return 0;
 };
