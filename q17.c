a9e88c3ae038040216562afa25ff2268c653b1fa
gabrielddrs <gabrieldavid.alu.lmb@gmail.com>
2024-10-26 16:58:12 -0300

    questao 17

--- q17.c
+++ q17.c
@@ -5,12 +5,12 @@ int main(){
     float raio;
     const float pi = 3.14159;
 
-    printf("Digite o raio do círculo: ");
+    printf("Digite o raio do círculo:\n");
     scanf("%f", &raio);
 
-    printf("Diâmetro: %f\n", 2 * raio);
-    printf("Circunferência: %f\n", 2 * pi * raio);
-    printf("Área: %f\n", pi * raio * raio);
+    printf("\nDiâmetro: %f\n", 2 * raio);
+    printf("\nCircunferência: %f\n", 2 * pi * raio);
+    printf("\nÁrea: %f\n", pi * raio * raio);
 
     return 0;
 }
