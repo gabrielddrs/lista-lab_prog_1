47a85e8dda56dd9651eb832bc52d5d876bf6c627
gabrielddrs <gabrieldavid.alu.lmb@gmail.com>
2024-10-31 22:38:37 -0300

    questao 21

---
+++ q21.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int main() {
+
+    // Letras maiúsculas
+    printf("A: %d\n", 'A');
+    printf("B: %d\n", 'B');
+    printf("C: %d\n", 'C');
+    printf("D: %d\n", 'D');
+    printf("E: %d\n", 'E');
+    printf("F: %d\n", 'F');
+    printf("G: %d\n", 'G');
+
+    // Letras minúsculas
+    printf("a: %d\n", 'a');
+    printf("b: %d\n", 'b');
+    printf("c: %d\n", 'c');
+    printf("d: %d\n", 'd');
+    printf("e: %d\n", 'e');
+    printf("f: %d\n", 'f');
+    printf("g: %d\n", 'g');
+
+    // Números
+    printf("0: %d\n", '0');
+    printf("1: %d\n", '1');
+    printf("2: %d\n", '2');
+    printf("3: %d\n", '3');
+    printf("4: %d\n", '4');
+    printf("5: %d\n", '5');
+    printf("6: %d\n", '6');
+
+    // Símbolos especiais
+    printf("$: %d\n", '$');
+    printf("*: %d\n", '*');
+    printf("+: %d\n", '+');
+    printf("/: %d\n", '/');
+    printf("@: %d\n", '@');
+    printf("&: %d\n", '&');
+
+    // Espaço em branco
+    printf("Espaço em branco: %d\n", ' ');
+
+    return 0;
+}
